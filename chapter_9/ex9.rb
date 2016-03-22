1f08567f556deb6d4b9698c46c3e581e84a5d23e
Richard Blankenhorn <rsbnq6@yahoo.com>
2016-03-21 23:12:00 -0600

    Modifying last puts statement to print a long string with 3 double quotes
    [#113796287]

--- chapter_9/ex9.rb
+++ chapter_9/ex9.rb
@@ -12,8 +12,10 @@ months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\n"
 puts "Here are the days: #{days}"
 puts "Here are the months: #{months}\n"
 
-# using \n to print a long string on to multiple lines.
-puts "There's something going on here.\n"\
-      "With the three double-quotes.\n"\
-      "We'll be able to type as much as we like.\n"\
-      'Even 4 lines if we want, or 5, or 6.'
+# using 3 double quotes to print a long string on to multiple lines.
+puts """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
